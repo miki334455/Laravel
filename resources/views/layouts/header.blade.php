9767785b9d0dde116eb790bf77490b1fcdccb16a
miki334455 <xograpesxo@gmail.com>
2023-06-29 23:03:47 +0900

    ヘッダーのナビゲーションのスタイルを変更

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-	<nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
+	<nav class="navbar fixed-top navbar-light bg-primary" style="height: 60px;">
 		<div class="container">
-			<a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>
+			<a href="{{ route('posts.index') }}" class="navbar-brand text-white fw-bold">投稿アプリ</a>
 		</div>
 	</nav>
 </header>
